9159fafedd0c726049f161b8d44df4d78c98e3c2
Misael <38136358+misaelrezende@users.noreply.github.com>
2019-04-08 08:50:59 -0300

    Consertado ex 5
    ex 7 esta incompleto

--- Capitulo 9/9.4.3/ex05.c
+++ Capitulo 9/9.4.3/ex05.c
@@ -4,24 +4,22 @@
     ---->>>>>>>>>  NAO ESTA PRONTO NAO  <<<<<<<<<<-----
 */
 
-int somaVetor(int v[], int n){
-    int soma = 0;
-    if(n == 0){
-        soma = v[n];
-        return soma;
-    }
-    printf("s=%d", soma);
-    return soma + somaVetor(v, n-1);
+int somatorioVetor(int *v, int n){
+	if(n == 0)
+		return v[0];
+	return v[n] + somatorioVetor(v, n-1);
 }
 
 int main(){
-    int n;
+	int i, n;
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &n);
-    int v[n], i;
+    int v[n];
+
     printf("Digite os elementos do vetor: ");
-    for(i = 0; i < n; i++)
-        scanf("%d", &v[i]);
-    printf("%d\n", somaVetor(v, n));
-    return 0;
+	for(i = 0; i < n; i++)
+		scanf("%d", &v[i]);
+	
+    printf("%d\n", somatorioVetor(v, n-1));
+	return 0;
 }
